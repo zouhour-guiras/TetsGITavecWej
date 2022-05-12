6b9bc98bf7148ca3c95b8747fa2b28838e2d3278
zouhour-guiras <guiras.zouhour@gmail.com>
2022-05-12 13:09:21 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TetsGITavecWej.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TetsGITavecWej.dir/main.c.obj"
+  "TetsGITavecWej.exe"
+  "TetsGITavecWej.exe.manifest"
+  "TetsGITavecWej.pdb"
+  "libTetsGITavecWej.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TetsGITavecWej.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
